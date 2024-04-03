4e902900800ef03f016cca937bd7bffa031e4bb6
iliyastb <skiliyas300@gmail.com>
2024-04-03 20:29:47 +0530

    Update

--- main.tf
+++ main.tf
@@ -97,21 +97,21 @@ resource "aws_lb_target_group" "main" {
   )
 }
 
-#resource "aws_lb_listener_rule" "listener_rule" {
-#  listener_arn = var.listener_rule
-#  priority     = var.listener_priority
-#
-#  action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.main.arn
-#  }
-#
-#  condition {
-#    host_header {
-#      values = [local.dns_name]
-#    }
-#  }
-#}
+resource "aws_lb_listener_rule" "listener_rule" {
+  listener_arn = var.listener_arn
+  priority     = var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.dns_name]
+    }
+  }
+}
 
 #resource "aws_route53_record" "main" {
 #  zone_id = data.aws_route53_zone.domain.zone_id
