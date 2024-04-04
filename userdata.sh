bd62ec0e5c9a72151935aca8668075c90c724c06
iliyastb <skiliyas300@gmail.com>
2024-04-04 15:35:53 +0530

    Update

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ls
-#ansible-pull -i localhost, -U https://github.com/iliyastb/robsohop-ansible.git roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
+
+ansible-pull -i localhost, -U https://github.com/iliyastb/robsohop-ansible.git roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
