d584690e246e193d51ae3e62d8da3f2114289d16
iliyastb <skiliyas300@gmail.com>
2025-07-16 20:33:17 +0530

    Update

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_launch_template" "main" {
   name                   = "${var.component}-${var.env}"
-#  image_id               = data.aws_ami.ami.id
-  image_id               = "ami-07d68e852b0dbe7e5"
+  image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
 
